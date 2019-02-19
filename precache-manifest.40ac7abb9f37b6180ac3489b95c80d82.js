021715a0f6e75e612562927f196f166021e0b9d1
Hannes Oldenburg <hannes.christian.oldenburg@gmail.com>
2019-02-19 20:44:15 +0000

    Updates

---
+++ precache-manifest.40ac7abb9f37b6180ac3489b95c80d82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c0d5431a37831efbdcd",
+    "url": "/dice/static/css/main.a0825e21.chunk.css"
+  },
+  {
+    "revision": "3c0d5431a37831efbdcd",
+    "url": "/dice/static/js/main.3c0d5431.chunk.js"
+  },
+  {
+    "revision": "7972f6a4b109b16cba52",
+    "url": "/dice/static/js/runtime~main.7972f6a4.js"
+  },
+  {
+    "revision": "78d930c755cf2390f11d",
+    "url": "/dice/static/js/2.78d930c7.chunk.js"
+  },
+  {
+    "revision": "d0708bf19946a233189a6cc44bb3b3a0",
+    "url": "/dice/index.html"
+  }
+];
